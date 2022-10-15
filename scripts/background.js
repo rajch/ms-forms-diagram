54ee27a876fb3938f9a1979cca13737cefeb9b38
Raj Chaudhuri <rajch@hotmail.com>
2022-10-15 23:03:31 +0530

    UI improvements

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,58 @@
 "use strict";
 
-const MSFORMSDESIGNPAGE = "https://forms.office.com/pages/designpagev2.aspx"
+const FORMSHOST = "forms.office.com"
+const PATHPREFIX1 = "/pages/designpagev2.aspx"
+const PATHPREFIX2 = "/Pages/DesignPageV2.aspx"
+const MSFORMSDESIGNPAGE = `https://${FORMSHOST}${PATHPREFIX1}`
+const MSFORMSDESIGNPAGE2 = `https://${FORMSHOST}${PATHPREFIX2}`
+
+chrome.runtime.onInstalled.addListener(
+    function extensionInstalled(details) {
+        chrome.action.disable();
+
+        chrome.declarativeContent.onPageChanged.removeRules(
+            undefined,
+            async function rulesAdding() {
+                let formsrule = {
+                    conditions: [
+                        new chrome.declarativeContent.PageStateMatcher({
+                            pageUrl: { hostSuffix: FORMSHOST, pathPrefix: PATHPREFIX1, queryContains: "subpage=design", schemes: ['https'] }
+                        }),
+                        new chrome.declarativeContent.PageStateMatcher({
+                            pageUrl: { hostSuffix: FORMSHOST, pathPrefix: PATHPREFIX2, queryContains: "subpage=design", schemes: ['https'] }
+                        })
+                    ],
+                    actions: [
+                        new chrome.declarativeContent.SetIcon({
+                            imageData: {
+                                16: await loadImageData("images/icon16.png"),
+                                32: await loadImageData("images/icon32.png"),
+                                48: await loadImageData("images/icon48.png"),
+                                128: await loadImageData("images/icon128.png")
+                            },
+                        }),
+                        new chrome.declarativeContent.ShowAction()
+                    ]
+                }
+
+                chrome.declarativeContent.onPageChanged.addRules([formsrule]);
+            }
+        )
+    }
+)
+
+async function loadImageData(url) {
+    const img = await createImageBitmap(await (await fetch(chrome.runtime.getURL(url))).blob());
+    const { width: w, height: h } = img;
+    const canvas = new OffscreenCanvas(w, h);
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, w, h);
+    return ctx.getImageData(0, 0, w, h);
+}
 
 chrome.action.onClicked.addListener(
     function actionClicked(tab) {
-        if (tab.url.includes(MSFORMSDESIGNPAGE)) {
+        if (tab.url.includes(MSFORMSDESIGNPAGE) || tab.url.includes(MSFORMSDESIGNPAGE2)) {
             chrome.scripting.executeScript(
                 {
                     target: { tabId: tab.id },
@@ -36,7 +84,7 @@ chrome.action.onClicked.addListener(
             );
         } else {
             // Wrong page
-            chrome.action.setBadgeText({tabId: tab.id, text:"NO"})
+            chrome.action.setBadgeText({ tabId: tab.id, text: "NO" })
         }
     }
 );
--- scripts/diagram.js
+++ scripts/diagram.js
@@ -6,25 +6,32 @@ function setElementText(elementid, text) {
     statuspanel.append(document.createTextNode(text))
 }
 
+function toggleopenbrancheditorpanel() {
+    const openbrancheditorpanel = document.getElementById("openbrancheditor")
+    const diagram = document.getElementById("diagram")
+    openbrancheditorpanel.classList.toggle("hidden")
+    diagram.classList.toggle("hidden")
+}
+
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
+    function (request, sender) {
         console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
             "from the extension:" + JSON.stringify(request));
-        //   if (request.greeting === "hello")
-        //     sendResponse({farewell: "goodbye"});
-        
+
         if(!request.status) {
-            setElementText("statuspanel","Error: I don't know what goes onabort.")
+            setElementText("statuspanel","Error: Wrong message sent.")
             return
         }
 
         if(request.status == "Error") {
             setElementText("statuspanel", "Error:" + request.error)
+            toggleopenbrancheditorpanel()
             return
         }
 
         if(request.status == "Success") {
+            setElementText("statuspanel", "")
             setElementText("diagram",request.diagramText)
             mermaid.init({ startOnLoad: false },".dynmermaid");
         }
