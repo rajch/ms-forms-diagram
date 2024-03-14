64b0b8518f6904fc58a2f52cd650e6e130c22354
Raj Chaudhuri <rajch@hotmail.com>
2024-03-14 13:29:15 +0530

    Changed mermaid rendering method, added copy image to clipboard

--- scripts/diagram.js
+++ scripts/diagram.js
@@ -3,27 +3,21 @@
 /* global mermaid, chrome */
 
 const diagramPage = {
-  self: undefined,
   downloadUrl: undefined,
   svgBlobUrl: undefined,
   diagramTitle: '',
   diagramText: '',
   diagramPrimaryColor: '#ececff',
   diagramStyle: 'basis',
-  diagramMode: 'default',
-  setTitle (diagramtitle) {
-    this.diagramTitle = diagramtitle
-    this.setElementText('diagramtitle', diagramtitle)
-  },
-  hideElement (elementid) {
+  hideElement(elementid) {
     const element = document.getElementById(elementid)
     element.classList.add('hidden')
   },
-  showElement (elementid, hide) {
+  showElement(elementid, hide) {
     const element = document.getElementById(elementid)
     element.classList.remove('hidden')
   },
-  setElementText (elementid, text) {
+  setElementText(elementid, text) {
     const element = document.getElementById(elementid)
 
     element.innerHTML = ''
@@ -31,64 +25,109 @@ const diagramPage = {
 
     return element
   },
-  setStatus (text) {
+  setDiagramTitle(diagramtitle) {
+    this.diagramTitle = diagramtitle
+    this.setElementText('diagramtitle', diagramtitle)
+  },
+  setStatus(text) {
     this.setElementText('statuspanel', text)
   },
-  setErrorStatus (text) {
-    this.setTitle('Error')
+  setErrorStatus(text) {
+    this.setDiagramTitle('Error')
     this.setStatus(text)
   },
-  setDiagramText (text) {
+  setDiagramText(text) {
     this.diagramText = text
 
     this.refreshDiagram()
   },
-  setDiagramStyle (stylename) {
+  setDiagramStyle(stylename) {
     this.diagramStyle = stylename
 
     this.refreshDiagram()
   },
-  diagramSource () {
-    return `---
-config:
-  theme: base
-  themeVariables:
-    primaryColor: "#ececff"
-    secondaryColor: "#e8e8e8"
-    primaryBorderColor: "${this.diagramPrimaryColor}"
-  flowchart:
-    curve: ${this.diagramStyle}
----
-${this.diagramText}`
+  diagramSource(noHTMLFlag) {
+    return [
+      '---',
+      'config:',
+      '  theme: base',
+      '  themeVariables:',
+      '    primaryColor: "#ececff"',
+      '    secondaryColor: "#e8e8e8"',
+      `    primaryBorderColor: "${this.diagramPrimaryColor}"`,
+      '  flowchart:',
+      `    curve: ${this.diagramStyle}`,
+      noHTMLFlag && '    htmlLabels: false' || '',
+      '---',
+      `${this.diagramText}`
+    ].join('\n')
   },
-  refreshDiagram () {
-    const diagramElement = this.setElementText('diagram', this.diagramSource())
+  refreshDiagram() {
+    const self = this
+
+    const diagramElement = document.getElementById('diagram')
     diagramElement.removeAttribute('data-processed')
 
-    mermaid.run({ nodes: [diagramElement], suppressErrors: true }).catch(
-      e => window.alert(e.message)
-    )
+    // The setTimeout is needed to let mermaid queue the render
+    setTimeout(() => {
+      mermaid.render('diagramSvg', self.diagramSource()).then((result) => {
+        diagramElement.innerHTML = result.svg
+      }).catch((error) => {
+        self.setErrorStatus(error)
+      })
+    }, 0)
+
   },
-  showDiagram (diagramtitle, diagramtext) {
-    if (diagramtitle) {
-      this.setTitle(diagramtitle)
-    }
-    if (diagramtext) {
-      this.setDiagramText(diagramtext)
-    } else {
-      this.refreshDiagram()
-    }
-  },
-  clearDiagram () {
-    const diagram = document.getElementById('diagram')
-    diagram.innerHTML = ''
-    diagram.removeAttribute('data-processed')
-  },
-  showOpenBranchEditorPanel () {
+  showOpenBranchEditorPanel() {
     this.showElement('openbrancheditor')
     this.hideElement('diagram')
   },
-  downloadImage (imageUrl) {
+  createPngBlob(callback) {
+    const self = this
+
+    mermaid.render('diagramPng', this.diagramSource(true))
+      .then((result) => {
+        const b = new Blob([result.svg], { type: 'image/svg+xml' })
+
+        if (self.svgBlobUrl) {
+          try {
+            URL.revokeObjectURL(self.svgBlobUrl)
+            self.svgBlobUrl = undefined
+          } catch { }
+        }
+        self.svgBlobUrl = URL.createObjectURL(b)
+
+        const img = new Image()
+        img.addEventListener('load', () => {
+          const diagramsvg = document.getElementById('diagramSvg')
+          if (!diagramsvg) {
+            return
+          }
+
+          // Create an ad-hoc canvas, same size as the SVG
+          const bbox = diagramsvg.getBBox()
+
+          const canvas = document.createElement('canvas')
+          canvas.width = bbox.width
+          canvas.height = bbox.height
+
+          const context = canvas.getContext('2d')
+
+          // Solid white background for non-transparent PNG
+          context.fillStyle = 'white'
+          context.fillRect(0, 0, canvas.width, canvas.height)
+
+          // Draw the image on the canvas
+          context.drawImage(img, 0, 0, bbox.width, bbox.height)
+
+          canvas.toBlob((blob) => {
+            callback(blob)
+          })
+        })
+        img.src = self.svgBlobUrl
+      }).catch(() => { })
+  },
+  downloadImage(imageUrl) {
     // Create a temporary link, configure it for download,
     // point it to the canvas-generated data url, and
     // 'click' it to trigger a download.
@@ -98,111 +137,49 @@ ${this.diagramText}`
       ? `${this.diagramTitle}.png`
       : 'image.png'
     a.download = downloadFileName
-    document.body.appendChild(a)
     a.href = imageUrl
     a.click()
     a.remove()
   },
-  createDownloadImage (sourceurl, diagramsvg, callback) {
-    const img = new Image()
-    img.addEventListener('load', () => {
-      // Create an ad-hoc canvas, same size as the SVG
-      const bbox = diagramsvg.getBBox()
-
-      const canvas = document.createElement('canvas')
-      canvas.width = bbox.width
-      canvas.height = bbox.height
-
-      const context = canvas.getContext('2d')
-
-      // Solid white background for non-transparent PNG
-      context.fillStyle = 'white'
-      context.fillRect(0, 0, canvas.width, canvas.height)
-
-      // Draw the image on the canvas
-      context.drawImage(img, 0, 0, bbox.width, bbox.height)
-
-      canvas.toBlob((blob) => {
-        if (this.downloadUrl) {
-          try {
-            URL.revokeObjectURL(this.downloadUrl)
-            this.downloadUrl = undefined
-          } catch { }
-        }
-        this.downloadUrl = URL.createObjectURL(blob)
-        this.downloadImage(this.downloadUrl)
-        if (callback) {
-          callback(this.downloadUrl)
-        }
-      })
-    })
-    img.src = sourceurl
+  styleDropdownChanged(e) {
+    const selectedstyle = e.target.value
+    this.setDiagramStyle(selectedstyle)
   },
-  setFallbackMode () {
-    this.diagramMode = 'fallback'
-    // Clean out diagram
-    this.clearDiagram()
+  copyButtonClicked(e) {
+    const self = this
 
-    // Re-render mermaid diagram without
-    // foreignObject elements
-    mermaid.initialize({
-      flowchart: {
-        htmlLabels: false
+    self.createPngBlob((blob) => {
+      try {
+        navigator.clipboard.write([
+          new ClipboardItem({
+            'image/png': blob
+          })
+        ])
+        window.alert('Diagram copied to clipboard.')
+      } catch (error) {
+        self.setErrorStatus(error)
       }
     })
-
-    // Reset diagram element
-    this.showDiagram(this.diagramTitle, this.diagramText)
-
-    // Wait a bit, and save it
+  },
+  copySourceButtonClicked(e) {
+    navigator.clipboard.writeText(this.diagramSource())
+    window.alert('Mermaid source copied to clipboard.')
+  },
+  saveButtonClicked(e) {
     const self = this
-    setTimeout(() => {
-      self.saveFallbackDiagram()
-    }, 2000)
-  },
-  saveFallbackDiagram () {
-    const diagram = document.getElementById('diagram')
-    const fallbacksvg = diagram.querySelector('svg')
-    const svgdata = (new XMLSerializer()).serializeToString(fallbacksvg)
-    const blob = new Blob([svgdata], { type: 'image/svg+xml' })
-    this.svgBlobUrl = URL.createObjectURL(blob)
 
-    console.log('Downloading from blob url')
-    this.createDownloadImage(this.svgBlobUrl, fallbacksvg)
-  },
-  saveButtonClicked (e) {
-    if (this.downloadUrl) {
+    if (self.downloadUrl) {
       console.log('Downloading from cached blob URL')
-      this.downloadImage(this.downloadUrl)
+      self.downloadImage(self.downloadUrl)
       return
     }
 
-    if (this.diagramMode === 'default') {
-      const diagramsvg = document.querySelector('pre.dynmermaid svg')
-      if (!diagramsvg) {
-        window.alert('Diagram not found.')
-        return
-      }
-
-      // First, convert the mermaid-generated svg into a data: url
-      // with base64 encoding
-      const svgdataUrl = 'data:image/svg+xml;base64,' +
-        btoa(diagramsvg.outerHTML)
-
-      console.log(`SIZE OF DATAURL:${svgdataUrl.length}`)
-      // Chrome silently fails if the data url size goes beyond a
-      // limit. The number 45000 is guesswork, based on tests.
-      if (svgdataUrl.length <= 45000) {
-        console.log('Downloading from data url')
-        this.createDownloadImage(svgdataUrl, diagramsvg)
-      } else {
-        this.setFallbackMode()
-      }
-    } else {
-      this.saveFallbackDiagram()
-    }
+    self.createPngBlob((blob) => {
+      self.downloadUrl = URL.createObjectURL(blob)
+      self.downloadImage(self.downloadUrl)
+    })
   },
-  visibilityChanged (e) {
+  visibilityChanged(e) {
     if (document.visibilityState === 'hidden') {
       if (this.downloadUrl) {
         URL.revokeObjectURL(this.downloadUrl)
@@ -215,7 +192,7 @@ ${this.diagramText}`
       console.log('Blobs cleared')
     }
   },
-  chromeMessageReceived (request, sender) {
+  chromeMessageReceived(request, sender) {
     if (!request || !request.status) {
       this.setErrorStatus(
         'Error: Wrong message sent. Please contact the developers.'
@@ -238,29 +215,35 @@ ${this.diagramText}`
         this.diagramPrimaryColor = request.themePrimaryColor.toLowerCase()
       }
 
-      this.showDiagram(request.diagramTitle, request.diagramText)
+      this.setDiagramTitle(request.diagramTitle)
+      this.setDiagramText(request.diagramText)
 
       const self = this
+
       const saveButton = document.getElementById('savebutton')
       saveButton.addEventListener('click', (e) => {
-        self.saveButtonClicked()
+        self.saveButtonClicked(e)
       })
+
+      const copybutton = document.getElementById('copybutton')
+      copybutton?.addEventListener('click', (e) => {
+        self.copyButtonClicked(e)
+      })
+
       const copysourcebutton = document.getElementById('copysourcebutton')
       copysourcebutton.addEventListener('click', (e) => {
-        navigator.clipboard.writeText(self.diagramSource())
-        window.alert('Mermaid source copied to clipboard.')
+        self.copySourceButtonClicked(e)
       })
+
       const styledropdown = document.getElementById('styledropdown')
       styledropdown.addEventListener('change', (e) => {
-        const selectedstyle = e.target.value
-        self.setDiagramStyle(selectedstyle)
-        this.showDiagram()
+        self.styleDropdownChanged(e)
       })
 
       this.showElement('savepanel')
     }
   },
-  init () {
+  init() {
     const self = this
 
     mermaid.initialize({ startOnLoad: false })
